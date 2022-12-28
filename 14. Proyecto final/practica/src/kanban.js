f7425d17458ea6173a6c2cdc9b5c1dcd195e9394
aasoto <andresalfredosotosuarez@gmail.com>
2022-12-28 15:47:56 -0500

    drag and drop con http

--- 14. Proyecto final/practica/src/kanban.js
+++ 14. Proyecto final/practica/src/kanban.js
@@ -41,6 +41,12 @@ export default class Kanban {
 
   removeCard (indexBoard, indexCard) {
     const card = this.getBoard(indexBoard).items.splice(indexCard, 1)[0]
+
+    get(`${this.url}/delete-card/${indexBoard}/${indexCard}`)
+    .then( response => {
+      console.log(response)
+    })
+
     return card
   }
 
@@ -51,20 +57,47 @@ export default class Kanban {
   moveCard (indexBoardSrc, indexCardSrc, indexBoardTarget, indexCardTarget) {
     const srcCard = this.removeCard(indexBoardSrc, indexCardSrc)
     this.insertCard(srcCard, indexBoardTarget, indexCardTarget)
+
+    post(`${this.url}/update-all`, 'json', {
+      boards: this.toJSON()
+    }).then( response => {
+      console.log(response)
+    })
   }
 
   updateBoard (id, index, title) {
     this.getBoard(index).title = title
+
+    post(`${this.url}/update-board`, 'json', {
+      id: id,
+      title: title
+    }).then( response => {
+      console.log(response)
+    })
   }
 
   removeBoard (index) {
     const id = this.boards[index].id
     this.boards.splice(index, 1)
+
+    get(`${this.url}/delete-board/${id}`)
+    .then( response => {
+      console.log(response)
+    })
   }
 
   updateCard (indexBoard, indexCard, title) {
     const card = this.boards[indexBoard].items[indexCard]
     card.title = title
+
+    
+    post(`${this.url}/update-card`, 'json', {
+      id: card.id,
+      title: title,
+      indexBoard: indexBoard
+    }).then( response => {
+      console.log(response)
+    })
   }
 
   async loadBoards () {
